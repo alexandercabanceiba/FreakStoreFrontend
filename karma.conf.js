a6678e5a263bffb1937cb8ffdc7dade64f3fcb05
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-06-22 11:58:00 -0500

    Se cambia configuración en Karma para chrome

--- karma.conf.js
+++ karma.conf.js
@@ -51,14 +51,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeNoSandbox'],
-
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
+    ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
-      }
-    },
+    }
+},
     singleRun: true,
     restartOnFileChange: true
   });
